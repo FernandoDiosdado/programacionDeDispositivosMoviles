748ee58ac9462dd9abdd834db378ad10f190b2a1
diosdado <luferradi2001@hotmail.com>
2025-02-18 20:43:11 -0600

    Tarea hecha

--- src/main/kotlin/holaKotlin.kt
+++ src/main/kotlin/holaKotlin.kt
@@ -1,6 +1,7 @@
 package mx.upiita.holakotlin // agregar el paqute al que pertenece
 
-fun main(){
+fun main(/*args: Array<String>*/){
+    //quitar comentario de arriba para bienvenida
     println("Hola mundo desde Kotlin!")
 
     val variable1 = 5.0 // tipo double
@@ -27,4 +28,53 @@ fun main(){
     println("Push hecho")
     println("Push hp hecho")
     println("Push regresado")
+
+    var arregloSimple = arrayOf(1,2,3,4)
+    println(arregloSimple[0])
+    println(arregloSimple[1])
+    println(arregloSimple[2])
+    println(arregloSimple[3])
+    //println(arregloSimple[4]) esto daria error porque el indice sale del limite
+    println(arregloSimple.joinToString()) // une todos los elementos de la cadena y los convierte en string
+
+    var arregloNulos: Array<Int?> = arrayOfNulls(5) // arreglo de enteros que puede contener nulos
+    println(arregloNulos.joinToString())
+
+    val datos = Array<Int>(10){i -> i*2} // crea un arreglo de tipo int e indica que para el indice i tiene un arregla de correspondencia
+
+    println(datos.joinToString())
+    datos.forEach { println(it) } // buscar diferencia entre este y labmda
+
+    var arregloPalabras = "Estas son varias palabras".split(" ").toTypedArray() // divide las palabras usando espacio, y .toTypedArray las guarda en un arreglo
+    arregloPalabras.forEach {elemento -> println(elemento) } // funcion lambda
+
+    val dosDArray = Array(3){Array<Int>(4) { 0 } } // arreglo de 3x4
+    dosDArray[1][3] = 8
+    println(dosDArray.joinToString()) // esto solo nos imprimiria las direcciones de los arreglos anidados
+    println(dosDArray.contentDeepToString())
+    println(dosDArray.forEach{element -> println(element.contentDeepToString()) })
+
+    var cadena: String = "CAdena de caracteres"
+    var rawString = """ Luis Cordero
+        Llegó tarde el día martes
+        a la superclase de ktlin
+        en donde Mariana se enojó """
+
+    var nombre = "Fulano "
+    var email = "fulano@micorreo.com"
+    var telefono = "+522222222222"
+
+    var concatenado = String.format("nombre: %s \nemail: %s\ntelefono: %s", nombre,email,telefono)
+    println(rawString)
+    println(concatenado)
+
+    //preferido en kotlin
+    var concatenado2 = "nombre: $nombre \nTeléfono: $telefono\nemain: $email"
+    println(concatenado2)
+
+    //imprimir usando argumentos de linea de comandos. Quitar comentarios para activiar
+    /*var _nombre = args[0]
+    var _apellido = args[1]
+    var concatenadoArgs = "Bienvenido de nuevo, $_nombre $_apellido"
+    println(concatenadoArgs)*/
 }
